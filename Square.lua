c5f99980c12ac7d10c9087910e7fc318e8ef4d06
Ryan Bridge <ryanbridgemusic@gmail.com>
2023-03-06 11:49:03 -0500

    Render squares to screen

---
+++ Square.lua
@@ -0,0 +1,20 @@
+Square = Class{}
+
+function Square:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    self.dy = 0
+    self.dx = 0
+end
+
+function Square:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Square:render()
+    love.graphics.rectangle('line', self.x, self.y, self.width, self.height)
+end
--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+Class = require 'class'
+require 'Square'
 
+MOVE_SPEED = 100
 
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -9,8 +12,19 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    love.window.setTitle('Squares')
+
+    math.randomseed(os.time())
+
+    -- initialize square objects
+    square1 = Square(100, WINDOW_HEIGHT / 2 - 37, 75, 75)
+    square2 = Square(WINDOW_WIDTH - 175, WINDOW_HEIGHT / 2 - 37, 75, 75)
 end
 
 function love.draw()
     love.graphics.clear(245/255, 40/255, 145/255, 255/255)
+
+    square1:render()
+    square2:render()
 end
